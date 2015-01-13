c7a767ce420dc3381a34d5f17c04b19f93ff7a3c
Tomaszpiotro <tomaszpiotro@gmail.com>
2015-01-13 21:46:02 +0100

    Initial commit.

---
+++ wow_server_online_tracker/wow_server_online_tracker/settings.py
@@ -0,0 +1,85 @@
+import os
+
+SETTINGS_ROOT = os.path.dirname(__file__)
+PROJECT_ROOT = os.path.abspath(os.path.join(SETTINGS_ROOT, os.pardir))
+PROJECT_DIR = SETTINGS_ROOT
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ['SECRET_KEY']
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = ['localhost',
+                 '127.0.0.1']
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'wow_server_online_tracker.urls'
+
+WSGI_APPLICATION = 'wow_server_online_tracker.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tpiotrowski',
+        'USER': 'postgres',
+        'PASSWORD': 'qwerty1asd',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    },
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.7/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+
+STATIC_ROOT = 'local_static/'
+
+STATIC_URL = '/local_static/'
+
+TEMPLATE_DIRS = (
+    '/home/sloth/Projects/inz/django/reports/reports_viewer/templates',
+)
