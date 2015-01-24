7073239117bfafa8e2b1318d9c00ef3e032d7f6d
anduril1 <tomaszpiotro@gmail.com>
2015-01-24 05:15:25 +0100

    Added charts app.

---
+++ wow_server_online_tracker/wow_charts/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+
+class Probe(models.Model):
+    datetime = models.DateTimeField(
+        verbose_name="datetime",
+        auto_now_add=True
+    )
+    value = models.IntegerField(
+        verbose_name="value"
+    )
+    series = models.ForeignKey(
+        Series,
+        verbose_name="series",
+        related_name="probes"
+    )
+
+    class Meta:
+        app_label = "wow_charts"
+
+
+class Series(models.Model):
+    name = models.CharField(
+        max_length=128,
+        verbose_name="name"
+    )
+
+    class Meta:
+        app_label = "wow_charts"
