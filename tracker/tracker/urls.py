7c140e84bd2e534c6ba09f7ba1c9120378f1fa79
tomaszpiotro <tomaszpiotro@gmail.com>
2015-02-12 05:41:34 +0100

    Added urls.

--- tracker/tracker/urls.py
+++ tracker/tracker/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = patterns(
     '',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^charts/', include('charts.urls')),
 )
+
+urlpatterns += staticfiles_urlpatterns()
