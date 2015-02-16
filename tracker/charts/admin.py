6af47abb84de01cd9928c58fc3d1dc93750c078b
tomaszpiotro <tomaszpiotro@gmail.com>
2015-02-16 22:03:37 +0100

    Registered Charts in admin.

--- tracker/charts/admin.py
+++ tracker/charts/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from charts.models import Probe
+from charts.models import Probe, Chart
 
 
 @admin.register(Probe)
 class HttpSeries(admin.ModelAdmin):
     list_display = ['date', 'value', 'series']
     list_filter = ['series__name']
+
+@admin.register(Chart)
+class ChartAdmin(admin.ModelAdmin):
+    list_display = ['title', 'type', 'x_axis_name', 'y_axis_name']
+    filter_horizontal = ['series']
