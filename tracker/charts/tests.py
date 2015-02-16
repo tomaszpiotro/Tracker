c9d6c7fb4c3b562edfbf39106727e941a9e29461
tomaszpiotro <tomaszpiotro@gmail.com>
2015-02-16 22:46:24 +0100

    Added ChartDetailView test with hours parameter.

--- tracker/charts/tests.py
+++ tracker/charts/tests.py
@@ -5,11 +5,20 @@ import autofixture
 
 
 class ChartTest(TestCase):
+    def setUp(self):
+        self.title = "title"
+        autofixture.create_one(Chart, field_values={'title': self.title,
+                                                    'slug': self.title})
+
     def test_simple_chart_detail_view(self):
-        title = "title"
-        autofixture.create_one(Chart, field_values={'title': title,
-                                                    'slug': title})
-        url = '/charts/' + title + "/"
+        url = '/charts/' + self.title + "/"
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, template_name='chart.html')
+
+    def test_chart_detail_view_with_hours(self):
+        url = '/charts/' + self.title + "/24"
         response = self.client.get(url)
 
         self.assertEqual(response.status_code, 200)
