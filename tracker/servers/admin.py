afbf60882187419979d16eaa7d53316f4ec3de49
tomaszpiotro <tomaszpiotro@gmail.com>
2015-02-17 23:25:43 +0100

    Registered new classes in admin.

--- tracker/servers/admin.py
+++ tracker/servers/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from .models import HttpSeries
+from servers.models import NestedSeries, ThirdNextSibling
 
 
 @admin.register(HttpSeries)
 class HttpSeries(admin.ModelAdmin):
     list_display = ['url', 'name', 'tag', 'html_id', 'html_class']
+
+
+@admin.register(NestedSeries)
+class NestedSeriesAdmin(HttpSeries):
+    pass
+
+
+@admin.register(ThirdNextSibling)
+class NestedSeriesAdmin(HttpSeries):
+    pass
